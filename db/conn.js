6243a34516a3919d7f8aa8d1718dfc9cc3ea4eb5
Pranav Jadhav <jadhavhpranav1121@gmail.com>
2022-05-10 16:34:24 +0530

    Backend node module reinstall

--- db/conn.js
+++ db/conn.js
@@ -2,7 +2,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://pizzaclient:KakashiHatake123@cluster0.udup1.mongodb.net/pizzaclient?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
+  const collection = client.db("pizzaclient").collection("devices");
   console.log('Connected');
   client.close();
 });
--- models/admin.js
+++ models/admin.js
@@ -10,7 +10,7 @@ const adminScheme = new mongoose.Schema({
         required: true,
         trim: true
     },
-    email: {
+    email: {    
         type: String,
         required: true,
         trim: true
