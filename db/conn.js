27f063b4fc5622dcb2be2d757df20aab1397cf66
Pranav Jadhav <jadhavhpranav1121@gmail.com>
2022-05-10 11:42:57 +0530

    Database changes

--- db/conn.js
+++ db/conn.js
@@ -1,10 +1,8 @@
-const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/pizzaclient", { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
-    .then(() => {
-        console.log("connection is successful");
-    })
-    .catch((err) => {
-        console.log(err);
-    });
-
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://pizzaclient:KakashiHatake123@cluster0.udup1.mongodb.net/pizzaclient?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('Connected');
+  client.close();
+});
--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Credentials", true);
     next();
 });
-app.use(cors({ origin: 'http://localhost:4200' }))
+app.use(cors({ origin: 'https://jadhavhpranav1121.github.io' }))
 app.use(adminRouter);
 app.use(customerRouter);
 app.use(itemsRouter);
