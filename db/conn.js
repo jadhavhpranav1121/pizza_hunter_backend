d97a5f3ea29e9a6d9a2e05fe985ddcceeeb3af9a
Pranav Jadhav <jadhavhpranav1121@gmail.com>
2022-12-17 22:29:36 +0530

    changes

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 const mongoose = require('mongoose');
-const uri = "mongodb+srv://pizzaclient:KakashiHatake123@cluster0.udup1.mongodb.net/pizzaclient?retryWrites=true&w=majority&ssl=true";
+const uri = `mongodb+srv://pizzaclient:${process.env.MONGO_PASSWORD}@cluster0.udup1.mongodb.net/pizzaclient?retryWrites=true&w=majority&ssl=true`;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true,useFindAndModify:false, useCreateIndex: true}).then(()=>{
     console.log("connection sucessful");
   }).catch((err)=>{
